21e6a2488438b4ecf04c81b0cb438deccb6413b6
Zhen <zhl146@gmail.com>
2017-07-01 20:22:44 -0400

    added gitignore for database

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,39 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(
+        r'^(?P<year>[\d]{4})/$',
+        views.get_post_summary_year,
+        name='get_post_summary_year'
+        ),
+    url(
+        r'^(?P<year>[\d]{4})/(?P<month>[\d]{2})/$',
+        views.get_post_summary_month,
+        name='get_post_summary_month'
+    ),
+    url(
+        r'^(?P<year>[\d]{4})/(?P<month>[\d]{2})/(?P<day>[\d]{2})/$',
+        views.get_post_summary_day,
+        name='get_post_summary_day'
+    ),
+    url(
+        r'^(?P<title_slug>[-\w]+)/$',
+        views.get_post,
+        name='get_post'
+    ),
+    url(
+        r'^category/(?P<category>[-\w]+)/$',
+        views.get_post_summaries_by_category,
+        name='get_post_summaries_by_category'
+    ),
+    url(
+        r'^category/(?P<tag>[-\w]+)/$',
+        views.get_post_summaries_by_tag,
+        name='get_post_summaries_by_tag'
+    ),
+    url(
+        r'',
+        views.get_post_summaries,
+        name='get_post_summaries'
+    ),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,35 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("I should return a blog post")
+def get_post(request, title_slug):
+    response = "I should return post named %s"
+    return HttpResponse(response % title_slug)
+
+
+def get_post_summary_year(request, year, num_posts = 5):
+    response = "I would return all post summaries in the year {}"
+    return HttpResponse(response.format(year))
+
+
+def get_post_summary_month(request, year, month, num_posts = 5):
+    response = "I would return all post summaries in {}/{}"
+    return HttpResponse(response.format(month, year))
+
+
+def get_post_summary_day(request, year, month, day, num_posts = 5):
+    response = "I would return all post summaries on {}/{}/{}"
+    return HttpResponse(response.format(month, day, year))
+
+
+def get_post_summaries(request, num_posts = 5):
+    return HttpResponse("I should return all post summaries")
+
+
+def get_post_summaries_by_category(request, category, num_posts = 5):
+    response = "I would return all post summaries with category {}"
+    return HttpResponse(response.format(category))
+
+
+def get_post_summaries_by_tag(request, tag, num_posts = 5):
+    response = "I would return all post summaries with tag {}"
+    return HttpResponse(response.format(tag))
