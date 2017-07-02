0d8b33a4136b4a2c6add906645833f479c7b18eb
Zhen <zhl146@gmail.com>
2017-07-01 22:13:57 -0400

    fixed limit by syntax for query

--- posts/views.py
+++ posts/views.py
@@ -4,22 +4,20 @@ from posts.models import Content, Tag
 
 
 def get_post(request, title_slug):
-    query_set = Content.objects.filter(slug=title_slug)
-    for item in query_set:
-        print(item.content)
-    return HttpResponse(query_set[0].content)
+    query_set = Content.objects.filter(slug=title_slug)[0]
+    return HttpResponse(query_set)
 
 
 def get_post_summary_year(request, year):
     posts_requested = request.GET.get('posts', 5)
-    query_set = Content.objects.filter(create_date__year=year)[posts_requested]
+    query_set = Content.objects.filter(create_date__year=year)[:posts_requested]
     return HttpResponse(query_set)
 
 
 def get_post_summary_month(request, year, month):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.filter(create_date__year=year,
-                                       create_date__month=month)[posts_requested]
+                                       create_date__month=month)[:posts_requested]
     return HttpResponse(query_set)
 
 
@@ -27,23 +25,23 @@ def get_post_summary_day(request, year, month, day):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.filter(create_date__year=year,
                                        create_date__month=month,
-                                       create_date__day=day)[posts_requested]
+                                       create_date__day=day)[:posts_requested]
     return HttpResponse(query_set)
 
 
 def get_post_summaries(request):
     posts_requested = request.GET.get('posts', 5)
-    query_set = Content.objects.order_by('-create_date')[posts_requested]
+    query_set = Content.objects.order_by('-create_date')[:posts_requested]
     return HttpResponse(query_set)
 
 
 def get_post_summaries_by_category(request, category):
     posts_requested = request.GET.get('posts', 5)
-    query_set = Content.objects.filter(category=category)[posts_requested]
+    query_set = Content.objects.filter(category=category)[:posts_requested]
     return HttpResponse(query_set)
 
 
 def get_post_summaries_by_tag(request, tag):
     posts_requested = request.GET.get('posts', 5)
-    query_set = Tag.objects.filter(tag_desc=tag)[posts_requested]
+    query_set = Tag.objects.filter(tag_desc=tag)[:posts_requested]
     return HttpResponse(query_set)
