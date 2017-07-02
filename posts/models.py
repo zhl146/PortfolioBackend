d57c2c8866eed838282fe8b153e3336a0341eb00
Zhen <zhl146@gmail.com>
2017-07-01 21:10:01 -0400

    fixed whitespace

--- posts/models.py
+++ posts/models.py
@@ -10,18 +10,24 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+
     def __str__(self):
         return self.last_name+", "+self.first_name+" ("+self.username+")"
 
+
 class Tag(models.Model):
     tag_desc = models.CharField(max_length=128)
+
     def __str__(self):
         return self.tag_desc
 
+
 class Category(models.Model):
     category_desc = models.CharField(max_length=128)
+
     def __str__(self):
         return self.category_desc
+
 
 class Content(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,5 +37,6 @@ class Content(models.Model):
     title = models.CharField(max_length=512)
     slug = models.SlugField(unique=True)
     content = models.TextField()
+
     def __str__(self):
         return self.author.last_name+" , "+self.author.first_name+": "+self.title
