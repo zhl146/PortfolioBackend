8a8dc88e39d0c07eaee142a68e86a4eaadde3a8b
Zhen <zhl146@gmail.com>
2017-07-01 22:40:27 -0400

    query lists are serialized to json before being returned

--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,27 @@
 from django.http import HttpResponse
-
+from django.core import serializers
 from posts.models import Content, Tag
 
 
 def get_post(request, title_slug):
-    query_set = Content.objects.filter(slug=title_slug)[0]
-    return HttpResponse(query_set)
+    post = Content.objects.filter(slug=title_slug)[0]
+    json = serializers.serialize('json', post)
+    return HttpResponse(json)
 
 
 def get_post_summary_year(request, year):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.filter(create_date__year=year)[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
 
 
 def get_post_summary_month(request, year, month):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.filter(create_date__year=year,
                                        create_date__month=month)[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
 
 
 def get_post_summary_day(request, year, month, day):
@@ -26,22 +29,26 @@ def get_post_summary_day(request, year, month, day):
     query_set = Content.objects.filter(create_date__year=year,
                                        create_date__month=month,
                                        create_date__day=day)[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
 
 
 def get_post_summaries(request):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.order_by('-create_date')[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
 
 
 def get_post_summaries_by_category(request, category):
     posts_requested = request.GET.get('posts', 5)
     query_set = Content.objects.filter(category=category)[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
 
 
 def get_post_summaries_by_tag(request, tag):
     posts_requested = request.GET.get('posts', 5)
     query_set = Tag.objects.filter(tag_desc=tag)[:posts_requested]
-    return HttpResponse(query_set)
+    json = serializers.serialize('json', query_set)
+    return HttpResponse(json)
